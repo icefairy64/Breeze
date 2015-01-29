53bd56cfa847ea4cd6fdc2a5809d49d224751e30
icefairy64 <ishido.uu@gmail.com>
2015-01-30 00:58:26 +0300

    Actors are now Entities and updatable Entities are Actors

---
+++ breeze/Game/Entity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Breeze.Game
+{
+    public class Saveable : Attribute
+    {
+    }
+
+    public partial class Entity
+    {
+        public Graphics.Drawable Image;
+        public string Name;
+        public int InstanceID;
+        protected double FX, FY;
+
+        public string InstanceStr
+        {
+            get { return Name + InstanceID.ToString(); }
+        }
+            
+        public double X
+        {
+            get { return FX; }
+            set
+            {
+                FX = value;
+                Image.X = (int)value;
+            }
+        }
+
+        public double Y
+        {
+            get { return FY; }
+            set
+            {
+                FY = value;
+                Image.Y = (int)value;
+            }
+        }
+
+        public Entity()
+        {
+            
+        }
+
+        public Entity(Graphics.Drawable image) : this()
+        {
+            Image = image;
+        }
+
+        ~Entity()
+        {
+            if (Image != null)
+                Graphics.Screen.FindLayer(Image.Layer).RemoveChild(Image);
+        }
+    }
+}
+
---
+++ breeze/Game/EntityReflection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Breeze.Game
+{
+    public partial class Entity
+    {
+        /// <summary>
+        /// Creates an instance of the specified entity type and sets provided parameters
+        /// </summary>
+        /// <param name="type">Type of instance</param>
+        /// <param name="par">Parameters to set</param>
+        public static Entity Build(Type type, BuildParams par)
+        {
+            if (type.IsSubclassOf(typeof(Entity)) == false)
+                throw new ReflectionException("Passed an non-entity type to the Entity.Build method");
+            Entity tmp = (Entity)Activator.CreateInstance(type);
+            foreach (KeyValuePair<String, object> pair in par)
+            {
+                ReflectionHelper.SetAnything(type, tmp, pair.Key, pair.Value);
+            }
+            return tmp;
+        }
+    }
+}
+
