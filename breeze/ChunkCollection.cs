f605cc3bc8e93fc8bbaf44b5f8f18814a7468670
icefairy64 <ishido.uu@gmail.com>
2015-02-08 01:37:28 +0300

    Add missing files

---
+++ breeze/ChunkCollection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Breeze
+{
+    public struct ChunkAddress
+    {
+        public int X;
+        public int Y;
+    }
+
+    public abstract class ChunkCollection<T>
+    {
+        public int ChunkSize { get; protected set; }
+        protected Dictionary<ChunkAddress, LinkedList<T>> Chunks;
+
+        protected ChunkCollection(int chunkSize)
+        {
+            ChunkSize = chunkSize;
+            Chunks = new Dictionary<ChunkAddress, LinkedList<T>>();
+        }
+
+        public LinkedList<T> GetChunkAt(int x, int y)
+        {
+            ChunkAddress addr = new ChunkAddress() { X = x / ChunkSize, Y = y / ChunkSize };
+
+            return Chunks.ContainsKey(addr) ? Chunks[addr] : null;
+        }
+
+        public abstract void ValidateChunkAt(int x, int y);
+
+        public void InsertAt(T item, int x, int y)
+        {
+            ChunkAddress addr = new ChunkAddress() { X = x / ChunkSize, Y = y / ChunkSize };
+
+            if (!Chunks.ContainsKey(addr))
+                Chunks.Add(addr, new LinkedList<T>());
+
+            Chunks[addr].AddLast(item);
+        }
+    }
+}
---
+++ breeze/Graphics/DrawableChunkCollection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Breeze.Graphics
+{
+    public class DrawableChunkCollection : ChunkCollection<Drawable>
+    {
+        public DrawableChunkCollection(int chunkSize)
+            : base(chunkSize)
+        {
+        }
+
+        public override void ValidateChunkAt(int x, int y)
+        {
+            ChunkAddress addr = new ChunkAddress() { X = x / ChunkSize, Y = y / ChunkSize };
+
+            if (!Chunks.ContainsKey(addr))
+                return;
+
+            var toRemove = new LinkedList<Drawable>();
+            var chunk = Chunks[addr];
+
+            foreach (Drawable e in chunk)
+            {
+                if (!addr.Equals(new ChunkAddress() { X = (int)e.X / ChunkSize, Y = (int)e.Y / ChunkSize }))
+                {
+                    Console.WriteLine("Addresses are not equal: {0}:{1} and {2}:{3}", addr.X, addr.Y, (int)e.X / ChunkSize, (int)e.Y / ChunkSize);
+                    toRemove.AddLast(e);
+                }
+            }
+
+            foreach (Drawable e in toRemove)
+            {
+                Console.WriteLine("Moving drawable");
+                chunk.Remove(e);
+                InsertAt(e, (int)e.X, (int)e.Y);
+            }
+        }
+    }
+}
+
